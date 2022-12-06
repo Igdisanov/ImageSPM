a5ebcb390feaf3b4ec67cceedf7bf4b608569a29
Vadim Igdisanov <Igidsanov@t-dev.pro>
2022-12-06 12:43:54 +0500

    add files for VIPER

---
+++ Sources/AllImageViewController/Router/AllImageRouter.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vadim Igdisanov on 06.12.2022.
+//
+
+import Foundation
---
+++ Sources/AllImageViewController/View/AllImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vadim Igdisanov on 06.12.2022.
+//
+
+import UIKit
+
+public class AllImageViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
